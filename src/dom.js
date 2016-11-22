a829cd7aa8ea1371e4b2ac9babf57d6918cb7ad1
Ali Kimbrell <alexandrakimbrell@gmail.com>
2016-11-21 21:55:47 -0600

    User input from DOM builds robots, attack function deducts weapons' damage on click and checks for game over, at game over console logs winner.

--- src/dom.js
+++ src/dom.js
@@ -3,15 +3,7 @@
 let robot = require('./quiz');
 let weapon = require('./weapons');
 
-let testBot = new robot.Viper();
-testBot.name = 'abc';
-testBot.setWeapon(new weapon.Grenade()); // PROMISE HERE
-testBot.getDamage();
-
-let testBot2 = new robot.Ninja();
-testBot2.name = 'xyz';
-testBot2.setWeapon(new weapon.Rocket());
-testBot2.getDamage();
+let userBot1, userBot2;
 
 let declareWinner = function(bot1, bot2){
 	if(bot1.health <= 0){
@@ -27,21 +19,58 @@ let checkGameOver = function(bot1, bot2){
 	}
 };
 
-let attack = function(bot1, bot2){
-	bot1 = testBot;
-	bot2 = testBot2;
-	console.log('bot1 @start', bot1);
-	console.log('bot2 @start', bot2);
-	bot1.health = bot1.health - bot2.damage;
-	bot2.health = bot2.health - bot1.damage;
-	console.log('bot1', bot1);
-	console.log('bot2', bot2);
-	checkGameOver(bot1, bot2);
+let buildBot = function(name, model, weapon){
+	let bot = null;
+	switch(model){
+		case 'viper':
+			bot = new robot.Viper();
+			break;
+		case 'ninja':
+			bot = new robot.Ninja();
+			break;
+	}
+	bot.name = name;
+	bot.setWeapon(selectWeapon(weapon));
+	bot.getDamage();
+	// console.log('bot', bot);
+	return bot;
+};
+
+let selectWeapon = function(weaponSelect){
+	let weaponChoice = null;
+	switch(weaponSelect){
+		case 'grenade':
+			weaponChoice = new weapon.Grenade();
+			break;
+		case 'rocket':
+			weaponChoice = new weapon.Rocket();
+			break;
+	}
+	return weaponChoice;
 };
 
 
+$("#bot1-weapon").change(function(){
+	userBot1 = buildBot($("#bot1-name").val(), $("#bot1-model").val(), $("#bot1-weapon").val());
+	return userBot1;
+});
 
+$("#bot2-weapon").change(function(){
+	userBot2 = buildBot($("#bot2-name").val(), $("#bot2-model").val(), $("#bot2-weapon").val());
+	return userBot2;	
+});
 
+$("#attack").click(function(){
 
+	console.log('userBot1 @start', userBot1);
+	console.log('userBot2 @start', userBot2);
 
-$("#attack").click(attack);
+	userBot1.health = userBot1.health - userBot2.damage;
+	userBot2.health = userBot2.health - userBot1.damage;
+
+	console.log('userBot1', userBot1);
+	console.log('userBot2', userBot2);
+
+	checkGameOver(userBot1, userBot2);
+
+});
