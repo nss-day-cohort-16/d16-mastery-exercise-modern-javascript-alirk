8521031d9d07ea3f11474f7d09c8d7e22c6488fb
Ali Kimbrell <alexandrakimbrell@gmail.com>
2016-11-21 20:05:40 -0600

    Attack button triggers attack function, checks for game over, declares winner in console.

--- src/dom.js
+++ src/dom.js
@@ -7,20 +7,41 @@ let testBot = new robot.Viper();
 testBot.name = 'abc';
 testBot.setWeapon(new weapon.Grenade()); // PROMISE HERE
 testBot.getDamage();
-// console.log(testBot);
 
 let testBot2 = new robot.Ninja();
 testBot2.name = 'xyz';
 testBot2.setWeapon(new weapon.Rocket());
 testBot2.getDamage();
 
+let declareWinner = function(bot1, bot2){
+	if(bot1.health <= 0){
+		console.log('Bot2 wins!');
+	} else if(bot2.health <= 0){
+		console.log('Bot1 wins!');
+	}
+};
+
+let checkGameOver = function(bot1, bot2){
+	if(bot1.health <= 0 || bot2.health <= 0){
+		declareWinner(bot1, bot2);
+	}
+};
+
 let attack = function(bot1, bot2){
+	bot1 = testBot;
+	bot2 = testBot2;
 	console.log('bot1 @start', bot1);
 	console.log('bot2 @start', bot2);
 	bot1.health = bot1.health - bot2.damage;
 	bot2.health = bot2.health - bot1.damage;
 	console.log('bot1', bot1);
 	console.log('bot2', bot2);
+	checkGameOver(bot1, bot2);
 };
 
-attack(testBot, testBot2);
+
+
+
+
+
+$("#attack").click(attack);
